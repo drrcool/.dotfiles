4f20719551c836412166a270c1204197c0e9ff15
Richard cool <rcool@netflix.com>
2022-12-29 06:22:30 -0500

    Finalish tweaks to config

--- .config/zsh/linux.zsh
+++ .config/zsh/linux.zsh
@@ -4,4 +4,5 @@ export GCM_CREDENTIAL_STORE=gpg
 alias volUp='pactl set-sink-volume @DEFAULT_SINK@ +2000'
 alias volDown='pactl set-sink-volume @DEAULT_SINK@ -2000'
 alias volMute='pactl set-sink-mute @DEFAULT_SINK@ toggle'
-alias emacs='LD_LIBRARY_PATH=~/repos/tree-sitter /usr/local/bin/emacs'
+export LD_LIBRARY_PATH=~/repos/tree-sitter/
+alias doomemacs='LD_LIBRARY_PATH=~/repos/tree-sitter /usr/local/bin/emacs --init-dir=~/.doomemacs.d'
