9adf31e02ca13e0656b3abedd17bcef36f661ee4
Richard Cool <richardjcool@gmail.com>
2022-12-28 20:05:36 -0800

    Wrapping up the installation

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,8 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" default))
  '(package-selected-packages
-   '(company-yasnippet dashboard null emacs-lisp-mode treemacs-magit company-box evil-nerd-commenter apheleia tsi counsel rainbow-mode lsp-treemacs popper which-key ef-themes lsp-ui doom-themes quelpa-use-package lua-mode perspective magit quickrun company modus-themes tree-sitter-langs diminish general treemacs-evil evil-org ivy-rich org-auto-tangle typescript-mode helpful kaolin-themes lsp-ivy yasnippet all-the-icons evil-collection web-mode rainbow-delimiters treemacs-projectile doom-modeline bufler undo-tree mixed-pitch)))
+   '(treemacs-all-the-icons treemacs-icons-dired treemacs-perspective evil-snipe evil-dvorak evil-visualstar evil-visualstart evil-exchange evil-surround gcmh electric-cursor centered-cursor-mode linum+ tide term-toggle ivy-prescient prescient org-projectile counsel-projectile company-yasnippet dashboard null emacs-lisp-mode treemacs-magit company-box evil-nerd-commenter apheleia tsi counsel rainbow-mode lsp-treemacs popper which-key ef-themes lsp-ui doom-themes quelpa-use-package lua-mode perspective magit quickrun company modus-themes tree-sitter-langs diminish general treemacs-evil evil-org ivy-rich org-auto-tangle typescript-mode helpful kaolin-themes lsp-ivy yasnippet all-the-icons evil-collection web-mode rainbow-delimiters treemacs-projectile doom-modeline bufler undo-tree mixed-pitch))
+ '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
