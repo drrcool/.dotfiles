82db84dbb8cdb54b235218d182a248563bd83211
Richard Cool <richardjcool@gmail.com>
2022-12-30 17:49:57 -0800

    chore: removed useless snippets

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
 	font = wezterm.font("Spleen 32x64"),
 	font_size = 20,
 	color_scheme = "Challenger Deep",
-	window_background_opacity = 0.99,
+	window_background_opacity = 0.98,
 	bold_brightens_ansi_colors = true,
 	automatically_reload_config = true,
 	hide_tab_bar_if_only_one_tab = true,
