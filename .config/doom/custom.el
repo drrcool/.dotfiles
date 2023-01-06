93f92df4c4870526f30e0182aadd635986932b32
Richard Cool <rcool@netflix.com>
2023-01-06 00:34:49 -0800

    fixed merge

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,8 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("30684c58675a5efc3c462411d077868625642e1f5046e6d1524e855df7d369c7" "d983956f195a1a5e920c78216e6c164f4fd73a4fdce3f532d527182409f96515" "38ab78e4efd20f018e8e963beb2d8c18029219b8c911a0064d16a621310cd487" "9ae0e81ced7c8d587cb1db9fcb528856315d352822082518884e8726fe681d1d" "25f79eb20039ac795a4b2b499c392934f424e59d75b1dd12680b340d962f4679" "c2cfd9ff8d15d002abdddf4df3f989e5a4f34dba4b702d7337b8879914c2f835" "830596655dc39879096d9b7772768de6042fb5a4293c6b90c98a9b98bce96e4a" "f4cdc8dea941e3c7e92b907e62cdc03e0483a350b738a43c2e118ce6be9880a6" default))
  '(org-agenda-files
-   '("~/org/projects.org" "/home/rcool/org/todo.org" "/home/rcool/org/agenda.org")))
+   '("/Users/rcool2/org/from-mobile.org" "/Users/rcool2/org/projects.org" "/Users/rcool2/org/journal.org" "/Users/rcool2/org/notes.org" "/Users/rcool2/org/inbox.org" "/Users/rcool2/org/todo.org" "/Users/rcool2/org/agenda.org"))
+ '(package-selected-packages
+   '(@ subatomic-theme spacemacs-theme noctilux-theme magit cyberpunk-theme cyberpunk-2019-theme color-theme-modern))
+ '(warning-suppress-types '((initialization) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
