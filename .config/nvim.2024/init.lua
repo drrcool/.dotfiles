151bc50a0bed8522cbcfeffd89fea6de2c377f27
Richard Cool <rcool@Richards-MacBook-Pro.local>
2024-01-02 07:00:39 -0800

    added new nvim config base

---
+++ .config/nvim.2024/init.lua
@@ -0,0 +1,17 @@
+require "config.options"
+require "config.lazy"
+
+if vim.fn.argc(-1) == 0 then
+	vim.api.nvim_create_autocmd("User", {
+		group = vim.api.nvim_crate_augroup("NeovimPDE", { clear = true}),
+		pattern = "VeryLazy",
+		callback = function()
+			require "config.autocmds"
+			require "config.keymaps"
+		end,
+	})
+else
+	require "config.autocmds"
+	require "config.keymaps"
+end
+
---
+++ .config/nvim.2024/lua/config/lazy.lua
@@ -0,0 +1,41 @@
+--- Install lazy.nvim
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Configure lazy.nvim
+require("lazy").setup {
+  spec = {
+    { import = "base" },
+    { import = "pde" },
+  },
+  defaults = { lazy = true, version = nil },
+  install = { missing = true, colorscheme = { "tokyonight", "catppuccin" } },
+  dev = { patterns = jit.os:find "Windows" and {} or { "alpha2phi" } },
+  checker = { enabled = true },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+}
