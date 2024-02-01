8a198e2c22003ce7d13f3aae7efa2c2e94a35eb2
Richard Cool <rcool@Richards-MacBook-Pro.local>
2024-02-01 11:13:03 -0800

    feature: added nvim optios

--- .config/lazynvim/lua/plugins/colorsTheme.lua
+++ .config/lazynvim/lua/plugins/colorsTheme.lua
@@ -1,12 +1,12 @@
 return {
   -- Add nightfox
   { "EdenEast/nightfox.nvim" },
-
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   -- Configure LaxyVim to load the colorscheme we want
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "carbonfox",
+      colorscheme = "moonfly",
     },
   },
 }
